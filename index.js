7aaa5ae1231c12fbc24700f673cc1f1ca7bbf365
usererke2010 <150028361+usererke2010@users.noreply.github.com>
2023-11-07 17:29:54 +0600

    html

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('my hw')
+let firstName = "John"; console.log(typeof firstName)
+let lastName = "Maguayer";
+let contry = "USA";
+let city = "New York";
+let Age = 39;
+let isMarried = "Yes";
+let year = 1984;
