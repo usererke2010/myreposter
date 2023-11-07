8f21cd1a5babf0853fb3ec22c261baaa82ae744f
usererke2010 <150028361+usererke2010@users.noreply.github.com>
2023-11-07 18:10:25 +0600

    js

--- index.js
+++ index.js
@@ -5,3 +5,32 @@ let city = "New York";  console.log(typeof city)
 let Age = 39;  console.log(typeof Age)
 let isMarried = "Yes";  console.log(typeof isMarried)
 let year = 1984;  console.log(typeof year)
+
+let DUI = prompt('what day is today?')
+let day = DUI.toLowerCase();
+
+switch(day){
+    case 'monday':
+        alert('monday')
+        break;
+    case 'tuesday':  
+        alert('tuesday') 
+        break;
+    case 'wednesday':
+        alert('wednesday')
+        break;
+    case 'thosday':
+        alert('thosday')
+        break;
+    case 'friday':
+        alert('friday')   
+        break;
+    case 'saturday':
+        alert( 'saturday')
+        break;
+    case 'sunday':
+        alert('sunday')
+        break;
+    default:
+        prompt('what day is today?')
+}
