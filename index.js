2b7a7d8f74a59d554d38a7ee7184bf48b4a1e09e
usererke2010 <150028361+usererke2010@users.noreply.github.com>
2023-11-08 17:02:53 +0600

    js

--- index.js
+++ index.js
@@ -1,36 +1,68 @@
-let firstName = "John"; console.log(typeof firstName)
-let lastName = "Maguayer";  console.log(typeof lastName)
-let contry = "USA";  console.log(typeof contry)
-let city = "New York";  console.log(typeof city)
-let Age = 39;  console.log(typeof Age)
-let isMarried = "Yes";  console.log(typeof isMarried)
-let year = 1984;  console.log(typeof year)
+// let firstName = "John"; console.log(typeof firstName)
+// let lastName = "Maguayer";  console.log(typeof lastName)
+// let contry = "USA";  console.log(typeof contry)
+// let city = "New York";  console.log(typeof city)
+// let Age = 39;  console.log(typeof Age)
+// let isMarried = "Yes";  console.log(typeof isMarried)
+// let year = 1984;  console.log(typeof year)
 
-let DUI = prompt('what day is today?')
-let day = DUI.toLowerCase();
+// let DUI = prompt('what day is today?')
+// let day = DUI.toLowerCase();
 
-switch(day){
-    case 'monday':
-        alert('monday')
-        break;
-    case 'tuesday':  
-        alert('tuesday') 
-        break;
-    case 'wednesday':
-        alert('wednesday')
-        break;
-    case 'thosday':
-        alert('thosday')
-        break;
-    case 'friday':
-        alert('friday')   
-        break;
-    case 'saturday':
-        alert( 'saturday')
-        break;
-    case 'sunday':
-        alert('sunday')
-        break;
-    default:
-        prompt('what day is today?')
+// switch(day){
+//     case 'monday':
+//         alert('monday')
+//         break;
+//     case 'tuesday':  
+//         alert('tuesday') 
+//         break;
+//     case 'wednesday':
+//         alert('wednesday')
+//         break;
+//     case 'thosday':
+//         alert('thosday')
+//         break;
+//     case 'friday':
+//         alert('friday')   
+//         break;
+//     case 'saturday':
+//         alert( 'saturday')
+//         break;
+//     case 'sunday':
+//         alert('sunday')
+//         break;
+//     default:
+//         prompt('what day is today?')
+// }
+
+// let myage = prompt('Enter you age')
+//////////////////////
+
+// if(age > 18){
+//  alert('You are old enough to drive.')
+// } else {
+//  alert('You are can`t drive with car')
+// }
+
+// let youage = 25;
+
+// if(myage > youage){
+// alert('You are older than me.')
+// } else {
+//     alert('I are older than you.')
+// }
+
+// let a = 3;
+// let b = 4;
+
+// if(a > b){
+//  alert('a big than b')
+// } else{
+//     alert('a small than b')
+// }
+
+let num = prompt('enter number!')
+
+if(num === Event){
+ alert('number this even')
 }
