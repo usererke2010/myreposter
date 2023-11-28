36d18a39a441a3c29e10c8e3c6d5affcdd79866e
usererke2010 <150028361+usererke2010@users.noreply.github.com>
2023-11-28 18:32:41 +0600

    js

---
+++ erkenur103.js
@@ -0,0 +1,21 @@
+//1 b
+//2 d
+//3 c
+//4 b
+//5 d
+//6 a
+//7 a
+//8 c
+//9 c
+//10 c
+//11 b
+//12 b
+//13 c
+//14 c
+//15 a
+
+function evaluateExpression(expression) {
+    return (expression);
+}
+
+console.log(evaluateExpression(3 * (4 + 2) / 7))
